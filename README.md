9498a993cbcd1abe51e30df044c7db7581af8fd2
MarcosTypeAP <sombromar@gmail.com>
2021-10-09 10:35:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Translates .Xmodmap file lines to bash lines</h1>
+
+<p>Useful for executing a .Xmodmap file on i3 windows manager.</p>
+
+<p>Use example: <code>python3 xmodmap-to-sh.py -s source/file/path destination/file/path</code></p>
---
+++ xmodmap-to-sh.py
@@ -0,0 +1,63 @@
+import sys
+import os
+
+
+def run():
+
+    try:
+        save = sys.argv[1] == "-s"
+        if save:
+            dest_file_path = sys.argv[len(sys.argv) - 1]
+            file_path = sys.argv[len(sys.argv) - 2]
+        else:
+            file_path = sys.argv[len(sys.argv) - 1]
+    except IndexError:
+        save = input("Save a new file? (y/N): ")
+        if "y" in save or "Y" in save:
+            save = True
+        else:
+            save = False
+
+        file_path = input("Source file path: ")
+        if save:
+            dest_file_path = input("Destination file path: ")
+
+    try:
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+    except (FileNotFoundError, NotADirectoryError):
+        print("Source file not found")
+        return
+
+    for i, line in enumerate(lines):
+        if line.startswith("keycode"):
+            lines[i] = 'xmodmap -e "{}"\n'.format(line.rstrip())
+            if not save:
+                os.system(lines[i])
+        elif line.startswith("!") and save:
+            lines[i] = "#" + line[1:]
+
+    if save:
+        try:
+            os.remove(dest_file_path)
+        except FileNotFoundError:
+            pass
+        except NotADirectoryError:
+            print("Invalid destination path")
+            return
+        except PermissionError:
+            print("Permission denied")
+            return
+
+        try:
+            with open(dest_file_path, "w") as file:
+                lines.insert(0, "#!/bin/bash\n")
+                lines.insert(1, "\n")
+                file.writelines(lines)
+        except PermissionError:
+            print("Permission denied")
+            return
+
+
+if __name__ == "__main__":
+    run()
